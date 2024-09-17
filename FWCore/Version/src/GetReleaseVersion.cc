5c162f88dcc0e83dba368b0aa7b53e93e6da81b0
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-09-17 16:51:36 +0200

    foo

--- FWCore/Version/src/GetReleaseVersion.cc
+++ FWCore/Version/src/GetReleaseVersion.cc
@@ -3,6 +3,8 @@
 #define STRINGIFY_(x_) #x_
 #define STRINGIFY(x_) STRINGIFY_(x_)
 
+
+
 namespace edm {
   std::string getReleaseVersion() {
     static std::string const releaseVersion(STRINGIFY(PROJECT_VERSION));
